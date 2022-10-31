c3fd9480dcae07c9bf06e38014782f2f293d0d6a
raybharadwaj <ngnr.ray@gmail.com>
2022-10-31 22:03:00 +0530

    added objectId validation to patch

--- src/routes/api/(tasks)/task/[id]/+server.js
+++ src/routes/api/(tasks)/task/[id]/+server.js
@@ -6,33 +6,39 @@ const collection = db.collection('tasks');
 
 /** @type {import('./$types').RequestHandler} */
 export async function PATCH ({ request, params }) {
-    let requestData = await request.json();
-    let taskObj = {
-        title: requestData.title,
-        completed: requestData.completed
-    }
-    try {
-        console.log(taskObj)
-        let response = await collection.updateOne(
-            { _id: ObjectId(params.id) },
-            { $set: taskObj }
-        );
+    if(ObjectId.isValid(params.id)) {
+        let requestData = await request.json();
+        let taskObj = {
+            title: requestData.title,
+            completed: requestData.completed
+        }
+        try {
+            console.log(taskObj)
+            let response = await collection.updateOne(
+                { _id: ObjectId(params.id) },
+                { $set: taskObj }
+            );
+            return json({
+                message: "Updated Test",
+                response
+            });
+        } catch(err) {
+            return json({
+                message: err.message
+            });
+        }
+    } else {
         return json({
-            message: "Updated Test",
-            response
-        });
-    } catch(err) {
-        return json({
-            message: err.message
+            message: "Invalid Object ID"
         });
     }
+
 }
 
 /** @type {import('./$types').RequestHandler} */
 export async function DELETE ({ params }) {
     if(ObjectId.isValid(params.id)) {
         try {
-            console.log("here");
             let response = await collection.deleteOne({
                 _id: ObjectId(params.id)
             })
